881cf2d51d746fed93dd74620b5889c143d74e00
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-12-06 14:32:54 +0100

    added tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+
+
+function LandingPage() {
+    return (
+        <div className="bg-gray-700">
+            <h1 className="text-white text-3xl font-bold underline">Welcome to the Landing Page</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
