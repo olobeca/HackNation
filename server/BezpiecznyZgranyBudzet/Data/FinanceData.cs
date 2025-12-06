b762852f90d2909a61e1079816ba96af25e1039f
QBA <bukaj.iksnimak@gmail.com>
2025-12-06 22:51:30 +0100

    switch to postgres

---
+++ server/BezpiecznyZgranyBudzet/Data/FinanceData.cs
@@ -0,0 +1,29 @@
+
+public class Finance_data
+{
+    public string Id { get; set; }
+    public string CzescBudzetowa { get; set; }
+    public string Dzial { get; set; }
+    public string Rozdzial { get; set; }
+    public string Paragraf { get; set; }
+    public string ZrodloFinansowania { get; set; }
+    public string GrupaWydatkow { get; set; }
+    public string BudzetZadaniowyPelny { get; set; }
+    public string BudzetZadaniowy { get; set; }
+    public string NazwaProgramu { get; set; }
+    public string KomorkaOrganizacyjna { get; set; }
+    public decimal PlanWI { get; set; }
+    public string DysponentSrodkow { get; set; }
+    public decimal Budzet { get; set; }
+    public string NazwaZadania { get; set; }
+    public string SzczegoloweUzasadnienie { get; set; }
+    public string Przeznaczenie { get; set; }
+    public decimal Potrzeby2026 { get; set; }
+    public decimal Limit2026 { get; set; }
+    public decimal BrakujacaKwota2026 { get; set; }
+    public decimal KwotaUmowy { get; set; }
+    public string NrUmowy { get; set; }
+    public string DotacjaZKim { get; set; }
+    public string PodstawaPrawnaDotacji { get; set; }
+    public string Uwagi { get; set; }
+}
