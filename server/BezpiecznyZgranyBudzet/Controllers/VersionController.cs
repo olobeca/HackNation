30c51b962136409940ab134ff3c195e35e695299
Wojciech-Kalota <wojciech.kalota1@gmail.com>
2025-12-07 08:58:05 +0100

    add validation structure

---
+++ server/BezpiecznyZgranyBudzet/Controllers/VersionController.cs
@@ -0,0 +1,33 @@
+﻿using BezpiecznyZgranyBudzet.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BezpiecznyZgranyBudzet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public VersionServices _versionServices;
+
+        public VersionController(VersionServices versionServices)
+        {
+            _versionServices = versionServices;
+        }
+
+        [HttpGet("versions")]
+        public async Task<IActionResult> GetVersions(Guid session)
+        {
+            var versions = await _versionServices.GetVersions(session);
+            return Ok(versions);
+        }
+
+        [HttpPost("rollback")]
+        public async Task<IActionResult> RollbackVersion(Guid session, int versionId)
+        {
+            await _versionServices.RollbackVersion(session, versionId);
+            return Ok();
+        }
+
+    }
+}
---
+++ server/BezpiecznyZgranyBudzet/Services/VersionServices.cs
@@ -0,0 +1,25 @@
+﻿using BezpiecznyZgranyBudzet.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BezpiecznyZgranyBudzet.Services
+{
+    public class VersionServices
+    {
+        private readonly IDbContextFactory<AppDbContext> _factory;
+
+        public VersionServices(IDbContextFactory<AppDbContext> factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task RollbackVersion(Guid session, int versionId)
+        {
+            return;
+        }
+
+        public async Task<List<int>> GetVersions(Guid session)
+        {
+            return new List<int>();
+        }
+    }
+}
