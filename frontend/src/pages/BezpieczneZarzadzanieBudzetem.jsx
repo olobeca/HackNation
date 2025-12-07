3cdc751615787a48e7dc2a6621092bfaf3371c1c
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-12-07 01:07:47 +0100

    added animation via Motion

--- frontend/src/pages/BezpieczneZarzadzanieBudzetem.jsx
+++ frontend/src/pages/BezpieczneZarzadzanieBudzetem.jsx
@@ -1,12 +1,44 @@
 import Header from '../components/Header';  
-
+import {useState} from 'react';
+import docsGray from '../assets/docsGray.svg';
+import docsWhite from '../assets/docsWhite.svg';
 
 function BezpieczneZarzadzanieBudzetem() {
 
+    const [isChanged,SetisChanged] = useState(false);
 
     return (
         <div className="bg-gray-50 h-screen">
             <Header/>
+            <div className="flex flex-col px-12">
+                <div className="w-full flex gap-5 items-center mt-5">
+                    <div className="bg-white shadow-md w-[65%] rounded-xl p-8 relative">
+                        {isChanged ? (
+                            <div className="flex flex-col gap-1 text-left ">
+                                <h1 className="text-sm text-gray-500">Akcje</h1>
+                                <h1 className="text-xs text-gray-400 mb-4">Zarządzaj zmianami w budżecie</h1>
+                                <button className="w-full py-4 rounded-xl bg-gray-100 text-gray-400 text-base cursor-not-allowed">Zapisz do Master-Repozytorium</button>
+                                <img src={docsGray} alt="dosc" className="w-8 h-8 absolute top-5 right-5 opacity-20"/>
+                            </div>
+                        ) : (
+                            <div className="flex flex-col gap-1 text-left ">
+                                <div className="flex justify-between items-center">
+                                    <div>
+                                        <h1 className="text-sm text-gray-500">Akcje</h1>
+                                        <h1 className="text-xs text-gray-400 mb-4">Zarządzaj zmianami w budżecie</h1>
+                                    </div>
+                                    <div className="bg-yellow-100 text-yellow-600 text-xs font-medium px-3 py-1 rounded-full motion-preset-pulse motion-duration-2000 ">Niezapisane zmiany</div>
+                                </div>
+                                <button className="w-full py-4 rounded-xl bg-green-600 hover:bg-green-700 hover:shadow-lg hover:scale-105 text-white text-base shadow-md">Zapisz do Master-Repozytorium</button>
+                                <img src={docsWhite} alt="dosc" className="w-8 h-8 absolute top-5 right-5 opacity-20"/>
+                            </div>
+                        )}
+                    </div>
+                    <div className="bg-white shadow-md rounded-xl p-5 w-full">
+
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
